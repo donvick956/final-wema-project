6c01966db9d3a2f214e3e183eb8cae936d43d2e9
ibrahim dotto jemila <jemiladotto@gmail.com>
2021-12-31 08:56:54 +0100

    error changes

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,6 +1,20 @@
+import "./Error.css";
+import { Link } from 'react-router-dom';
+import ErrorPic from '../assets/ErrorPic.png';
+
+// var sectionStyle ={
+//     width: "100%",
+//     height: "400px",
+//     backgroundImage: "url(" + { ErrorPic } + ")"
+// };
+
 const Error = () => {
-    return (<div>
-        Error Works!!!!
+    return (
+    <div className = 'error-404'>
+        <div style={{ backgroundImage: `url(${ErrorPic})` }}></div>
+        <div>
+        <Link to="/"><button className="button-home">Go Home</button></Link>
+        </div>
     </div>)
 }
 export default Error;
